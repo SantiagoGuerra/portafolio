b473947c751d518563226ab6adb51f7e3d3b1295
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-23 23:13:13 -0500

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
