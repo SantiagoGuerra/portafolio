e56ec35d724d8d341cbbfa6b7921cd2743d84561
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-24 19:46:41 -0500

    Add css ruled

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,13 @@ const config = {
         use: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ],
   },
   plugins: [
