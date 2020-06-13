09f035440914b98821d08175382cfc3c643af427
Santiago Guerra <guerrasantiago19@gmail.com>
2020-06-12 23:24:20 -0500

    Add header layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/styles.css';
+import 'remixicon/fonts/remixicon.css';
 import data from './data.json';
 
 import Header from './layout/Header';
 
 
-document.querySelector('.main').appendChild(Header(data.profile.info))
+document.querySelector('.main').appendChild(Header(data.profile.info));
 
 console.log(Header(data.profile.info));
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,27 +1,32 @@
 import { elementFromString } from '../utils/index';
 
 export default function Header(data) {
+  const socialMediaHelper = data => {
+    const list = data.map(socialNetwork => `<li> <a href="${socialNetwork.url}" class="social-link ${socialNetwork.brand}" target="_BLANK"> <i class="ri-${socialNetwork.brand}-line"></i> </a> </li>`).join(' ');
+
+    console.log(list);
+    return list;
+  };
 
 
+  const name = elementFromString(`<h1 class="title">${data.name.toUpperCase()}</h1>`);
+  const role = elementFromString(`<p class="subtitle">${data.role}</p>`);
+  const description = elementFromString(`<p class="text-center description">${data.description}</p>`);
+  const socialMedia = elementFromString(`<ul class="social-icons">${socialMediaHelper(data.social)}</ul>`);
 
 
-const name = elementFromString(`<h1 class="title">${data.name.toUpperCase()}</h1>`);
-const role = elementFromString(`<p class="subtitle">${data.role}</p>`);
-const description = elementFromString(`<p class="text-center description">${data.description}</p>`);
-const socialMedia = elementFromString(`${data.social.map(socialNetwork => `<ul class="social-icons"><li> <a href="${socialNetwork.url}" class="social-link ${socialNetwork.brand}" target="_BLANK"> <i data-feather="${socialNetwork.brand}"></i> </a> </li></ul>`)}`)
-const messageMe = elementFromString('<a href="mailto:guerrasantiago19@gmail.com" class="btn" target="_BLANK">Message me!</a>')
+  const messageMe = elementFromString('<a href="mailto:guerrasantiago19@gmail.com" class="btn" target="_BLANK">Message me!</a>');
 
 
-  const header = document.createElement('header')
-  header.classList.add('header')
+  const header = document.createElement('header');
+  header.classList.add('header');
 
   header.appendChild(name);
   header.appendChild(role);
   header.appendChild(description);
   header.appendChild(socialMedia);
+  header.appendChild(messageMe);
 
-
-  
 
   return header;
 }
